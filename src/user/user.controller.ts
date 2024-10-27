4ded67295b144df67e6a67d8ff8b75066262dad3
munene-m <munenenjue18@gmail.com>
2024-10-27 12:49:24 +0300

    chore: use the role guard to protect certain endpoints to only be accessed by specific user roles

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,30 @@
-import { Body, Controller, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { LoginUserDto } from './dto/login-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('user')
+import { LoginUserDto } from './dto/login-user.dto';
+import { CheckRoleGuard } from 'src/common/guards/check-role.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '@prisma/client';
+
+@Controller('users')
+@UseGuards(CheckRoleGuard)
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('engineer/create')
+  @Roles(Role.ADMIN)
   async createUser(@Body() data: CreateUserDto) {
     return this.userService.createEngineer(data);
   }
@@ -29,11 +46,38 @@ export class UserController {
     };
   }
   @Post('project-manager/create')
+  @Roles(Role.ADMIN)
   async createProjectManager(@Body() data: CreateUserDto) {
     return this.userService.createProjectManager(data);
   }
   @Post('login')
   async loginUser(@Body() data: LoginUserDto) {
     return this.userService.loginUser(data);
+  }
+
+  @Get(':id')
+  @Roles(Role.ADMIN, Role.PROJECT_MANAGER)
+  async getUserById(@Param('id') id: number) {
+    return this.userService.getUserById(id);
+  }
+  @Get('')
+  @Roles(Role.ADMIN, Role.PROJECT_MANAGER)
+  async getAllUsers() {
+    return this.userService.getAllUsers();
+  }
+
+  @Put('edit/:id')
+  @Roles(Role.ADMIN)
+  async updateUser(
+    @Param('id') id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.updateUser(id, updateUserDto);
+  }
+
+  @Delete(':id')
+  @Roles(Role.ADMIN)
+  async deleteUser(@Param('id') id: number) {
+    return this.userService.deleteUser(id);
   }
 }
